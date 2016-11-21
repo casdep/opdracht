a95f179ff6ea6b2146621638cc9b7f4b9e3799b5
Cas de Pender <cas_de_pender@hotmail.com>
2016-11-21 11:44:03 +0100

    verbeterd

--- src/trio/opdracht/TrioOpdracht.java
+++ src/trio/opdracht/TrioOpdracht.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trio.opdracht;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 /**
  *
- * @author Mko98
+ * @author Cas
  */
-public class TrioOpdracht {
-
-    /**
-     * @param args the command line arguments
-     */
+public class TrioOpdracht extends JFrame{
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame frame = new TrioOpdracht();
+        frame.setSize (750,200);
+        frame.setLocation(400,280);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Trio opdacht");
+        frame.setContentPane( new TrioPaneel());
+        frame.setVisible(true);
     }
-    
 }
