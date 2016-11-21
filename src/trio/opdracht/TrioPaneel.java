0efab118c1783fb923d02d207737d142625a5c8f
Cas de Pender <cas_de_pender@hotmail.com>
2016-11-21 13:48:40 +0100

    11-21-13:48

--- src/trio/opdracht/TrioPaneel.java
+++ src/trio/opdracht/TrioPaneel.java
@@ -12,6 +12,7 @@ import java.awt.GridLayout;
 import javafx.scene.control.ComboBox;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -20,14 +21,17 @@ import javax.swing.JPanel;
  */
 class TrioPaneel extends JPanel {
     private JPanel paneelCenter, paneelZuid, paneelWest;
+    private JLabel centerLabel, zuidLabel;
     private ComboBox serie;
     private JButton overzicht1, overzicht2, overzicht3, overzicht4;
     public TrioPaneel() {
+        setLayout (new BorderLayout());
+        setBackground(Color.GREEN);
+        
         paneelCenter = new JPanel();
         paneelZuid = new JPanel();
         paneelWest = new JPanel();
-        
-        setBackground(Color.GREEN);
+
         
         paneelCenter.setLayout(new GridLayout(1,4));
         paneelZuid.setLayout(new BoxLayout(paneelZuid, BoxLayout.X_AXIS));
@@ -40,16 +44,21 @@ class TrioPaneel extends JPanel {
         overzicht3 = new JButton(" Overzicht 3");
         overzicht4 = new JButton(" Overzicht 4");
         
+        centerLabel = new JLabel (" Extra test voor Center paneel");
+        zuidLabel =  new JLabel (" Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.")
+        
+        add(paneelWest, BorderLayout.WEST);
         add(paneelCenter, BorderLayout.CENTER);
         add(paneelZuid, BorderLayout.SOUTH);
-        add(paneelWest, BorderLayout.WEST);
         
         
+        paneelCenter.add(centerLabel);
+        paneelZuid.add(zuidLabel);
         
         paneelWest.add(overzicht1);
         paneelWest.add(overzicht2);
-        paneelWest.add(overzicht2);
-        paneelWest.add(overzicht2);
+        paneelWest.add(overzicht3);
+        paneelWest.add(overzicht4);
    }
     
 }
